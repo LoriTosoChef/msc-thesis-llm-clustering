ca1d372ff63b579c0cdaa10d529864332e037699
LoriTosoChef <lorenzo.tosoni.main@gmail.com>
2023-03-17 17:53:12 +0100

    add get reddit posts

--- bots/reddit.py
+++ bots/reddit.py
@@ -1,10 +1,12 @@
+from typing import List
+
 import logging
 import praw
 
 logger = logging.getLogger(__name__)
 for logger_name in ("praw", "prawcore"):
     reddit_logger = logging.getLogger(logger_name)
-    reddit_logger.setLevel(logging.DEBUG)
+    reddit_logger.setLevel(logging.INFO)
 
 class RedditBot:
     def __init__(self,
@@ -24,3 +26,23 @@ class RedditBot:
                                   client_secret=self.client_secret,
                                   redirect_url=self.redirect_url,
                                   user_agent=user_agent)
+        
+    def get_reddit_posts(self,
+                         subreddit_list: List[str],
+                         limit: int = 10) -> List[dict]:
+        
+        try:
+            logger.debug(f'Receiving {limit} reddit posts')
+            posts_data = self.client.subreddit(display_name=subreddit_list).top(time_filter='all', limit=limit)
+        except Exception as e:
+            logger.warning(f'Could not get recent reddit posts, returning empty dicts - {e}')
+            return [{}]
+        
+        posts = []
+        for post in posts_data:
+            d = {}
+            d['post_id'] = post.id
+            d['text'] = post.title + '\n' + post.selftext
+            posts.append(d)
+            
+        return posts
