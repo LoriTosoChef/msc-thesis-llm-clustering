7326d843c19ac160a772713c86f3844fb520cc75
LoriTosoChef <lorenzo.tosoni.main@gmail.com>
2023-03-17 11:58:56 +0100

    add reddit init

---
+++ bots/reddit.py
@@ -0,0 +1,23 @@
+import logging
+import praw
+
+logger = logging.getLogger(__name__)
+
+class RedditBot:
+    def __init__(self,
+                 client_id: str,
+                 client_secret: str,
+                 redirect_url: str,
+                 user_agent: str = 'MyApp 0.0.1') -> None:
+        
+        logger.debug('Creating reddit bot instance')
+        
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_url = redirect_url
+        self.user_agemt = user_agent
+        
+        self.client = praw.Reddit(client_id=self.client_id,
+                                  client_secret=self.client_secret,
+                                  redirect_url=self.redirect_url,
+                                  user_agent=user_agent)
