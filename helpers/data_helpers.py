eceda7a3ebbf802800494474f023c1b7840c8507
LoriTosoChef <lorenzo.tosoni.main@gmail.com>
2023-03-20 15:44:14 +0100

    add reading and saving text helpers

--- helpers/data_helpers.py
+++ helpers/data_helpers.py
@@ -1,4 +1,5 @@
 import logging
+import re
 import pandas as pd
 
 from datetime import datetime
@@ -12,8 +13,24 @@ def save_to_parquet(data_dir: str, df: pd.DataFrame, name: str) -> None:
     month = str(datetime.now().month)
     year = str(datetime.now().year)
     
-    date_str = year + '_' + month + '_' + day
+    date_str = year + month + day
     
     df.to_parquet(f'{data_dir}/{name}_{date_str}.parquet', index=False)
+    
+    return
+
+
+def clean_text(text: str) -> str:
+    text = re.sub(r"(?:\@|http?\://|https?\://|www)\S+", "", text)
+    text = re.sub("@[A-Za-z0-9_]+","", text)
+    
+    return text
+
+
+def save_to_text(df: pd.DataFrame, col: str, out_dir: str, name: str) -> None:
+    all_text = ' '.join(df[col])
+    
+    with open(f'{out_dir}/{name}.txt', 'w') as f:
+        f.write(all_text)
     
     return
