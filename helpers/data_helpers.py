9fa884659320a76323ec6c64a3cf7094b4420e51
LoriTosoChef <lorenzo.tosoni.main@gmail.com>
2023-03-20 16:29:51 +0100

    tweets cleaning and saving txt

--- helpers/data_helpers.py
+++ helpers/data_helpers.py
@@ -4,11 +4,28 @@ import pandas as pd
 
 from datetime import datetime
 
-from config import DATA_DIR
-
 logger = logging.getLogger(__name__)
 
+
+def clean_tweets_df(df: pd.DataFrame, text_col: str, n: int) -> pd.DataFrame:
+    logger.debug('Dropping tweets with less than {n} words')
+    logger.debug('Adding starter word "Tweet"')
+    
+    df['splits'] = df[text_col].str.split()
+    df['len_splits'] = df['splits'].map(lambda x: len(x))
+    
+    df = df.loc[df['len_splits'] >= n].copy()
+    
+    df['full_text'] = '\nTWEET: ' + df[text_col]
+    
+    df.drop(columns=['splits', 'len_splits', text_col], inplace=True).reset_index(drop=True, inplace=True)
+    
+    return df
+
+
 def save_to_parquet(data_dir: str, df: pd.DataFrame, name: str) -> None:
+    logger.debug('Saving {name} to .parquet...')
+    
     day = str(datetime.now().day)
     month = str(datetime.now().month)
     year = str(datetime.now().year)
@@ -21,8 +38,13 @@ def save_to_parquet(data_dir: str, df: pd.DataFrame, name: str) -> None:
 
 
 def clean_text(text: str) -> str:
+    logger.debug('Cleaning text...')
+    
+    text = ' '.join(text.split())
+    
     text = re.sub(r"(?:\@|http?\://|https?\://|www)\S+", "", text)
     text = re.sub("@[A-Za-z0-9_]+","", text)
+    text = re.sub('  ', '', text)
     
     return text
 
@@ -32,5 +54,7 @@ def save_to_text(df: pd.DataFrame, col: str, out_dir: str, name: str) -> None:
     
     with open(f'{out_dir}/{name}.txt', 'w') as f:
         f.write(all_text)
+    
+    logger.info(f'Saved corpus to .txt file: {name}.txt')
     
     return
