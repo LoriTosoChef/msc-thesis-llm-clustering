151ee598c87db0eb37bd9b02b0ebfefcfbb52718
LoriTosoChef <lorenzo.tosoni.main@gmail.com>
2023-03-28 16:36:01 +0200

    add docstrings and normalize tweet text

--- helpers/data_helpers.py
+++ helpers/data_helpers.py
@@ -8,16 +8,20 @@ logger = logging.getLogger(__name__)
 
 
 def clean_tweets_df(df: pd.DataFrame, text_col: str, n: int) -> pd.DataFrame:
+    """Helper function to clean dataframes containing tweets"""
+    
     logger.debug('Dropping tweets with less than {n} words')
     logger.debug('Adding starter word "Tweet"')
     
+    # drop tweets with less than n words
     df['splits'] = df[text_col].str.split()
     df['len_splits'] = df['splits'].map(lambda x: len(x))
-    
     df = df.loc[df['len_splits'] >= n].copy()
     
+    # Add starter word
     df['full_text'] = '\nTWEET: ' + df[text_col]
     
+    # Clean dataset
     df = df.drop(columns=['splits', 'len_splits', text_col])
     df = df.reset_index(drop=True)
     
@@ -25,6 +29,7 @@ def clean_tweets_df(df: pd.DataFrame, text_col: str, n: int) -> pd.DataFrame:
 
 
 def save_to_parquet(data_dir: str, df: pd.DataFrame, name: str) -> None:
+    """Helper function to save dataframe to parquet file"""
     logger.debug('Saving {name} to .parquet...')
     
     day = str(datetime.now().day)
@@ -39,18 +44,25 @@ def save_to_parquet(data_dir: str, df: pd.DataFrame, name: str) -> None:
 
 
 def clean_text(text: str) -> str:
+    """Helper function to clean and normalize tweets text"""
     logger.debug('Cleaning text...')
     
     text = ' '.join(text.split())
     
+    # removing urls
     text = re.sub(r"(?:\@|http?\://|https?\://|www)\S+", "", text)
+    # removing mentions
     text = re.sub("@[A-Za-z0-9_]+","", text)
+    # removing excess spaces
     text = re.sub('  ', '', text)
+    # normalize text
+    text = text.lower()
     
     return text
 
 
 def save_to_text(df: pd.DataFrame, col: str, out_dir: str, name: str) -> None:
+    """Helper function to convert dataframe to txt file"""
     all_text = ' '.join(df[col])
     
     with open(f'{out_dir}/{name}.txt', 'w') as f:
