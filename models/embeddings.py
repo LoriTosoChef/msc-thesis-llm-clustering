8be8eda431f17731a027f31038640fce5b120669
LoriTosoChef <lorenzo.tosoni.main@gmail.com>
2023-04-13 18:04:51 +0200

    add logger

--- models/embeddings.py
+++ models/embeddings.py
@@ -1,4 +1,5 @@
 import logging
+import config
 
 from sentence_transformers import SentenceTransformer
 import gensim.downloader as api
@@ -10,13 +11,18 @@ class SentenceEmbeddings:
     def __init__(self, name: str) -> None:
         self.model_name = name
         
+        logger.info(f'Initializing {self.model_name.upper()} for Sentence Embeddings')
+        
         if 'mpnet' in self.model_name:
+            logger.debug('Loading all-mpnet-base-v2 model')
             self.model = SentenceTransformer('all-mpnet-base-v2')
         elif 'distilroberta' in self.model_name:
+            logger.debug('Loading all-distilroberta-v1')
             self.model = SentenceTransformer('all-distilroberta-v1')
             
     
-    def generate_embeddings(self, text):
+    def generate_embeddings(self, text: list):
+        logger.info(f'{self.model_name} - Generating sentence embeddings...')
         return self.model.encode(text)
     
 
@@ -24,11 +30,16 @@ class WordEmbeddings:
     def __init__(self, name: str) -> None:
         self.model_name = name
         
+        logger.info(f'Initializing {self.model_name.upper()} for Word Embeddings')
+        
         if 'w2v-google' in self.model_name:
+            logger.debug('Loading word2vec-google-news-300')
             self.model = api.load('word2vec-google-news-300')
         elif 'glove-twitter' in self.model_name:
+            logger.debug('Loading glove-twitter-200')
             self.model = api.load('glove-twitter-200')
         elif 'glove-twitter' in self.model_name:
+            logger.debug('Loading glove-wiki-gigaword-300')
             self.model = api.load('glove-wiki-gigaword-300')
     
     
