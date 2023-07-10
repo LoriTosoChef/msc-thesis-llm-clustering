41b562464e06480e7b5b60497c0736b93b1f9cb9
LoriTosoChef <lorenzo.tosoni.main@gmail.com>
2023-07-10 18:48:06 +0200

    final plot funciton

--- helpers/data_viz.py
+++ helpers/data_viz.py
@@ -13,50 +13,63 @@ logger = logging.getLogger(__name__)
 
 
 def plot_clusters_2D(df: pd.DataFrame,
-                     model: str,
+                     llm: str,
                      cluster_algo: str,
-                     save: bool,
-                     fig_name: str):
+                     text_col: str,
+                     n: int = 5,
+                     indices: list = None,
+                     manual: bool = False,
+                     save_fig: bool = False,
+                     fig_name: str = None):
+   
+   # init plot
+   fig, ax = plt.subplots(1,1,figsize=(15,7))
+   # Apply t-SNE to reduce embeddings to 2D
+   tsne = TSNE(n_components=2, random_state=42)
+   embeddings_2d = tsne.fit_transform(np.array(df[f'{llm}_embeddings'].tolist()))
+   # create a new DataFrame for 2D embeddings and cluster labels
+   embeddings_df = pd.DataFrame(embeddings_2d, columns=['x', 'y'])
+   embeddings_df['label'] = df[f'{llm}_{cluster_algo}'].values
+   if text_col == 'tweet':
+      embeddings_df['text'] = df['full_text'].values
+   elif text_col == 'output':
+      embeddings_df['text'] = df[llm].values
+   #color mapping
+   labels = embeddings_df['label'].unique()
+   colors = ['lightpink', 'lightblue', 'lightgreen', 'lightred', 'green', 'red', 'yellow']
+   color_dict = dict(zip(labels, colors))
+   # create labels
+   for label in labels:
+      temp = embeddings_df[embeddings_df['label'] == label]
+      ax.scatter(temp['x'], temp['y'], c=color_dict[label], label=label)
 
-    # dimensionality reduction
-    tsne = TSNE(n_components=2, random_state=42)
-    embeddings_2d = tsne.fit_transform(np.array(df[f'{model}_embeddings'].to_list()))
-    # init data temp df and plot
-    temp_df = pd.DataFrame({'x': embeddings_2d[:, 0], 'y': embeddings_2d[:, 1], 'label': df[f'{model}_{cluster_algo}'], 'text': df[f'{model}'], 'tweet': df['full_text']})
-    fig, axs = plt.subplots(1, 2, figsize=(15, 7))
-    # set colors
-    colors = ['lightpink', 'lightblue', 'lightgreen']
-    for ax, column in zip(axs, ['text', 'tweet']):
-        scatter = ax.scatter(temp_df['x'], temp_df['y'], c=temp_df['label'].apply(lambda label: colors[label]), alpha=0.5)
-        # subsample of labels to plot
-        subset_labels = temp_df.groupby('label').sample(n=100)
-        # K-means clustering on the subset of points to find points that are far from each other
-        kmeans = KMeans(n_clusters=4)
-        kmeans.fit(subset_labels[['x', 'y']])
-        centroids = kmeans.cluster_centers_
-        # find the closest point to each centroid and use it for annotation
-        highlighted_points = []
-        for centroid in centroids:
-            closest_point = ((subset_labels['x'] - centroid[0])**2 + (subset_labels['y'] - centroid[1])**2).idxmin()
-            highlighted_points.append(closest_point)
+   # KMeans on the embeddings to select four labels that are far from each other
+   kmeans = KMeans(n_clusters=n, random_state=42)
+   kmeans_label = kmeans.fit_predict(embeddings_2d)
 
-        # second plot for highlighted data points
-        highlighted_df = temp_df.loc[highlighted_points]
-        highlighted_scatter = ax.scatter(highlighted_df['x'], highlighted_df['y'], c=highlighted_df['label'].apply(lambda label: colors[label]), edgecolor='black', s=25)
-        # create labels
-        for i, point in highlighted_df.iterrows():
-            words = point[column].split()[:18]
-            label_lines = [' '.join(words[i:i+4]) for i in range(0, len(words), 6)]
-            label = '\n'.join(label_lines + ['...\nCluster: ' + str(point['label'])])
+   if manual:
+      highlighted_df = embeddings_df.loc[indices]
+   else:
+      # four datapoints closest to the kmeans cluster centers
+      indices = []
+      for center in kmeans.cluster_centers_:
+         distance = np.sum((embeddings_2d - center)**2, axis=1)
+         indices.append(np.argmin(distance))
+      highlighted_df = embeddings_df.loc[indices]
+      ax.scatter(highlighted_df['x'], highlighted_df['y'], c=highlighted_df['label'].apply(lambda label: color_dict[label]), edgecolor='black', s=25)
+      
+   # create labels
+   for i, point in highlighted_df.iterrows():
+      words = point['text'].split()[:18]
+      label_lines = [' '.join(words[i:i+4]) for i in range(0, len(words), 6)]
+      label = '\n'.join(label_lines + ['...\nCluster: ' + str(point['label'])])
 
-            ax.annotate(label, (point['x'], point['y']))
-
-    plt.tight_layout()
-    if save:
+      ax.annotate(label, (point['x'], point['y']))
+      
+   plt.legend()
+   if save_fig:
         plt.savefig(f'fig/{fig_name}.png', dpi=300)  # Save with a high DPI value
-    plt.show()
-    
-    return
+   plt.show()
 
 
 def plot_wordcloud(df: pd.DataFrame,
@@ -79,5 +92,3 @@ def plot_wordcloud(df: pd.DataFrame,
         if save:
             plt.savefig(f'fig/wordcloud_{model}_{clustering_algo}.png', dpi=300)
         plt.show()
-        
-        return
