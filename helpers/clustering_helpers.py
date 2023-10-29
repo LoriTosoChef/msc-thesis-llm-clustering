62923ac61eca2d786246fc2f3f2c9a67bab7026e
LoriTosoChef <lorenzo.tosoni.main@gmail.com>
2023-10-29 15:57:04 +0100

    add bertopic and silhouette score per cluster

--- helpers/clustering_helpers.py
+++ helpers/clustering_helpers.py
@@ -2,9 +2,12 @@ from typing import List
 
 import logging
 import numpy as np
+import pandas as pd
 
 from models.clustering import ClusteringModel
-from sklearn.metrics import calinski_harabasz_score, davies_bouldin_score, silhouette_score
+from sklearn.metrics import silhouette_score, silhouette_samples
+
+from bertopic import BERTopic
 
 logger = logging.getLogger(__name__)
 
@@ -33,7 +36,8 @@ def dbscan_loop(data,
                 dbscan.fit_predict(embeddings=data, pca_flag=True, n_components=n_components)
 
                 try:
-                    scores = silhouette_score(data, dbscan.clusters)
+                    #scores = silhouette_score(data, dbscan.clusters)
+                    scores = silhouette_score(dbscan.embeddings, dbscan.clusters)
                 except Exception as e:
                     logger.debug(e)
                     scores = -np.inf
@@ -74,7 +78,8 @@ def kmeans_loop(data,
                 kmeans.fit_predict(embeddings=data, pca_flag=True, n_components=n_components)
 
                 try:
-                    scores = silhouette_score(data, kmeans.clusters, metric='euclidean')
+                    #scores = silhouette_score(data, kmeans.clusters, metric='euclidean')
+                    scores = silhouette_score(kmeans.embeddings, kmeans.clusters, metric='euclidean')
                 except Exception as e:
                     logger.debug(e)
                     scores = -np.inf
@@ -101,7 +106,7 @@ def get_best_scores(results: dict, model_name: str):
         best_scores['max_iter'] = res_array['max_iter'][best_score_index]
         best_scores['n_clusters'] = res_array['n_clusters'][best_score_index]
         
-        logger.info(f"Best Score: {best_scores['score']}")
+        logger.info(f"Best Score: {best_scores['score']} - Clusters: {best_scores['n_clusters']}")
         
         return best_scores
     
@@ -117,9 +122,56 @@ def get_best_scores(results: dict, model_name: str):
             best_scores['eps'] = res_array['eps'][best_score_index]
             best_scores['n_clusters'] = res_array['n_clusters'][best_score_index]
             
-            logger.info(f"Best Score: {best_scores['score']}")
+            logger.info(f"Best Score: {best_scores['score']} - Clusters: {best_scores['n_clusters']}")
             
             return best_scores
         except Exception as e:
             logger.warning(e)
             return {}
+
+
+def silhouette_score_per_cluster(df: pd.DataFrame, algo: str):
+    sample_sil_gpt = silhouette_samples(df[f'gpt-3.5-turbo_pca_emb_{algo}'].tolist(), df[f'gpt-3.5-turbo_{algo}'])
+    sample_sil_alpaca = silhouette_samples(df[f'alpaca_pca_emb_{algo}'].tolist(), df[f'alpaca_{algo}'])
+    sample_sil_gpt4all = silhouette_samples(df[f'gpt4all_pca_emb_{algo}'].tolist(), df[f'gpt4all_{algo}'])
+
+    means_list_gpt = {}
+    means_list_alpaca = {}
+    means_list_gpt4all = {}
+    
+    unique_cl_gpt = df[f'gpt-3.5-turbo_{algo}'].unique()
+    unique_cl_alpaca = df[f'alpaca_{algo}'].unique()
+    unique_cl_gpt4all = df[f'gpt4all_{algo}'].unique()
+    
+    for cluster in unique_cl_gpt:
+        means_list_gpt[str(cluster)] = sample_sil_gpt[df[f'gpt-3.5-turbo_{algo}'] == cluster].mean()
+    for cluster in unique_cl_alpaca:
+        means_list_alpaca[str(cluster)] = sample_sil_alpaca[df[f'alpaca_{algo}'] == cluster].mean()
+        
+    if algo == 'dbscan':
+        return means_list_gpt, means_list_alpaca
+    
+    for cluster in unique_cl_gpt4all:
+        means_list_gpt4all[str(cluster)] = sample_sil_gpt4all[df[f'gpt4all_{algo}'] == cluster].mean()
+    
+    return means_list_gpt, means_list_alpaca, means_list_gpt4all
+
+
+def get_topics(df: pd.DataFrame, llm: str, algo: str):
+    clusters = df[f'{llm}_{algo}'].unique()
+    
+    topics_per_cluster = {}
+
+    for cluster in clusters:
+        df_cluster = df[df[f'{llm}_{algo}'] == cluster]
+        
+        topic_model = BERTopic()
+        topics, _ = topic_model.fit_transform(df_cluster[f'{llm}'])
+        topics_series = pd.Series(topics)
+
+        most_frequent_topic = topics_series.value_counts().idxmax()
+        topic_words = topic_model.get_topic(most_frequent_topic)
+        
+        topics_per_cluster[cluster] = topic_words
+        
+        logger.info(f'{cluster}: {topic_words}')
